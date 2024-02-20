ec75a3bffbeae358d4d646d15be30ef48a97683c
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-02-20 18:17:31 +0100

    Added AppNavBar component.

--- web/src/layouts/DashboardLayout.vue
+++ web/src/layouts/DashboardLayout.vue
@@ -7,7 +7,7 @@
     </div>
 </template>
 <script setup lang="ts">
-import AppSidebar from '@/components/layouts/AppSidebar.vue';
+import AppSidebar from '@/components/layouts/AppNavbar.vue';
 import AppTopBar from '@/components/layouts/AppTopBar.vue';
 </script>
 <style lang="scss" scoped>
