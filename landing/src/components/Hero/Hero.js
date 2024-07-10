b8e42f6ea8dc0d8f779498947c26836f24c62b49
Ajayi Olamide Daniel <prodbyola@gmail.com>
2024-07-10 12:14:05 +0100

    landing(fix) Moved Hero image assets to public folder to fix 'images not rendering on vercel'

--- landing/src/components/Hero/Hero.js
+++ landing/src/components/Hero/Hero.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './Hero.scss';
-import backgroundPattern from './assets/Background-pattern.svg';
-import dashboard from './assets/dashboard.svg';
-import table from './assets/table.svg';
-import profileBar from './assets/profile-bar.svg';
-import vUp from './assets/v-up.svg';
-import vDown from './assets/v-down.svg';
-import colorBar from './assets/Vector.svg';
+// import backgroundPattern from './assets/Background-pattern.svg';
+// import dashboard from './assets/dashboard.svg';
+// import table from './assets/table.svg';
+// import profileBar from './assets/profile-bar.svg';
+// import vUp from './assets/v-up.svg';
+// import vDown from './assets/v-down.svg';
+// import colorBar from './assets/Vector.svg';
+
+const getImage = (name) => '/images/hero/'+name+'.svg'
 
 const Hero = () => {
   return (
     <section id="hero" className="hero">
-      <div className="grid-pattern" style={{ backgroundImage: `url(${backgroundPattern})` }}></div>
-      <img src={colorBar} alt="Color Bar" className="color-bar-image" />
+      <div className="grid-pattern" style={{ backgroundImage: `url(${getImage('Background-pattern')})` }}></div>
+      <img src={getImage('Vector')} alt="Color Bar" className="color-bar-image" />
       <div className="hero-content">
         <h1>
           <span className="highlight">Unleash</span> <span>the Power <br/> of Your Data</span>
@@ -21,12 +23,11 @@ const Hero = () => {
         <button className="btn primary-btn">Join Waiting List</button>
       </div>
       <div className="hero-image">
-        <img src={dashboard} alt="Dashboard" className="dashboard-image" />
-        <img src={table} alt="Table" className="table-image" />
-        <img src={profileBar} alt="Profile Bar" className="profile-bar-image" />
-        <img src={vDown} alt="Visualize Bar Down" className="v-down-image" />
-        <img src={vUp} alt="Visualize Bar Up" className="v-up-image" />
-        
+        <img src={getImage('dashboard')} alt="Dashboard" className="dashboard-image" />
+        <img src={getImage('table')} alt="Table" className="table-image" />
+        <img src={getImage('profile-bar')} alt="Profile Bar" className="profile-bar-image" />
+        <img src={getImage('v-down')} alt="Visualize Bar Down" className="v-down-image" />
+        <img src={getImage('v-up')} alt="Visualize Bar Up" className="v-up-image" />
       </div>
     </section>
   );
