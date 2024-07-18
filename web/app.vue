eb0c6b9539efed93614c996de933594ed62fe1c4
DeathKnight813 <mullar000117@hotmail.com>
2024-07-19 03:04:08 +0900

    test commit

--- web/app.vue
+++ web/app.vue
@@ -3,7 +3,7 @@
     <UCard class="mt-10">
       <template #header>
         <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter!</h1>
+          <h1>Welcome to Nuxt UI Starter</h1>
           <ColorScheme
             ><USelect
               v-model="$colorMode.preference"
