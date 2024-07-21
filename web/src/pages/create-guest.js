e89bf110689fc130d656d0cb55474bfa3674b81e
DeathKnight813 <mullar000117@hotmail.com>
2024-07-22 02:10:25 +0900

    web project inited with react and material UI

---
+++ web/src/pages/create-guest.js
@@ -0,0 +1,13 @@
+import '../styles/create-guest.scss';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+function CreateGuest() {
+  return (
+    <div className="create-guest-container">
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
+
+export default CreateGuest;
